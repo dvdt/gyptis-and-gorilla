2704f8737c851c4a61789274db31c2d4306c4c9c
David Tsao <davidtsao@gmail.com>
2016-04-28 01:09:17 -0500

    worksheet for getting gorilla repl to use gyptis plotting

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject gor "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [gyptis "0.2.2"]]
+  :plugins [[lein-gorilla "0.3.6"]])
